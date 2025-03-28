f70d1219eaae37f707688f168f67506237ef5e66
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2025-03-28 07:53:20 +0000

    Implemented basis of front-end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'
+import './app.css'
+
+import logo from './assets/logo.png'
+
+function App()
+{
+    const [poll, setPoll] = useState({});
+
+    useEffect(() => 
+        { 
+            const url = new URL("http://localhost:5000/polls/1");
+            fetch(url).then(response => response.json()).then(data => { setPoll(data.poll) }).catch(error => { console.log(error); });
+        }, [])
+
+    return (
+        <>
+        <img id="logo" alt="logo" src={logo} />
+        <h1>{poll.question}</h1>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
